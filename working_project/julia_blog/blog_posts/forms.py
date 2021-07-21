bb5b3457ac66ccf9ccc8e8baa8931bcee11c7a30
JuliaS-L1 <65658639+JuliaS-L1@users.noreply.github.com>
2021-07-21 12:03:28 +0100

    fix validators

--- working_project/julia_blog/blog_posts/forms.py
+++ working_project/julia_blog/blog_posts/forms.py
@@ -1,9 +1,9 @@
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField
+from wtforms import StringField, SubmitField, TextAreaField,validators
 from wtforms.validators import DataRequired
 
 class BlogPostForm(FlaskForm):
-    blog_title = StringField('Title',validators=[DataRequired()])
-    blog_text = TextAreaField('Text',validators=[DataRequired])
+    blog_title = StringField('Title',[validators.DataRequired()])
+    blog_text = TextAreaField('Text',[validators.DataRequired()])
     submit = SubmitField('Create')
--- working_project/julia_blog/core/forms.py
+++ working_project/julia_blog/core/forms.py
@@ -1,11 +1,11 @@
 #used for submit questions
 #crud
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField
+from wtforms import StringField, SubmitField, TextAreaField,validators
 from wtforms.validators import DataRequired
 
 class ContactForm(FlaskForm):
-    email = StringField('Email',validators=[DataRequired()])
-    contact_text = TextAreaField('Text',validators=[DataRequired])
+    email = StringField('Email',[validators.DataRequired()])
+    contact_text = TextAreaField('Text',[validators.DataRequired()])
     submit = SubmitField('Send')
 #Image??
